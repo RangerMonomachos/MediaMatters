dcea3f2565a39e4b7e7776d2d18e75910c90afb4
JamesEdwinGentry <ranger@theevergreenhearth.org>
2020-06-04 14:15:21 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MediaMatters.Controllers
+{
+    public class HomeController: Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Albums()
+        {
+            return View();
+        }
+        public IActionResult Books()
+        {
+            return View();
+        }
+        public IActionResult Movies()
+        {
+            return View();
+        }
+    }
+}
